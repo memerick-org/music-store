ec1653b742198c28d21146710daa905d25b975aa
Will <admin@memerick.org>
2025-02-28 22:06:56 -0700

    finalizing main branch

--- src/view/Main.java
+++ src/view/Main.java
@@ -4,15 +4,9 @@
 
 package view;
 
-import java.util.ArrayList;
-
-import model.Album;
-
 public class Main {
     public static void main(String[] args) {
         MusicStore tester = new MusicStore();
-        ArrayList<Album> test = tester.store.getAllAlbums();
-        System.out.println(test.isEmpty());
         tester.mainLoop();
     }
 }
